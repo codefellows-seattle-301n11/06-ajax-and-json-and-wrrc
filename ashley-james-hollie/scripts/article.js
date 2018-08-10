fc1b286961ebeb987f5456e98c3cd2af9a77ea04
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-08-09 19:36:39 -0700

    Added README file

--- ashley-james-hollie/scripts/article.js
+++ ashley-james-hollie/scripts/article.js
@@ -27,21 +27,21 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-// REVIEW: There are some other functions that also relate to all articles across the board, rather than just single instances. Object-oriented programming would call these "class-level" functions, that are relevant to the entire "class" of objects that are Articles.
+// REVIEWED: There are some other functions that also relate to all articles across the board, rather than just single instances. Object-oriented programming would call these "class-level" functions, that are relevant to the entire "class" of objects that are Articles.
 
-// REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
+// REVIEWED: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
-// COMMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE Raw data is from the JSON file, loadAll is called in the fetchAll function that is from local storage. So one is from the JSON server and the other was from local storage. 
+// COMMMENTED: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// PUT YOUR RESPONSE HERE: rawData is from the JSON file, loadAll is called in the fetchAll function that is from local storage. So one is from the JSON server and the other was from local storage.
 Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
   articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
 }
 
-// REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
+// REVIEWED: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
 Article.fetchAll = () => {
-  // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
+  // REVIEWED: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
     Article.loadAll(JSON.parse(localStorage.rawData));
   } else {
